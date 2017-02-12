0cc860faa159873709435448152f5d560995a20b
Tom Bonner <tom@bonner.is>
2017-02-12 19:21:43 +0000

    Moved Arg to public

--- src/Phunk/Luan/Arg.cs
+++ src/Phunk/Luan/Arg.cs
@@ -3,7 +3,7 @@ using Phunk.Values;
 
 namespace Phunk.Luan
 {
-    internal class Arg : Tuple<string, Value>
+    public class Arg : Tuple<string, Value>
 	{
 		public Arg(string item1, Value item2) : base(item1, item2)
 		{
